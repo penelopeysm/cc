f220b03b2698779c7deabc3e60e255e289b3d355
Penelope Yong <penelopeysm@gmail.com>
2026-04-11 21:04:26 +0100

    fix web app

--- web/main.ml
+++ web/main.ml
@@ -1,5 +1,11 @@
 open Cccc
 open Js_of_ocaml
 
-let compile_source (source : string) : string = "TODO"
+let compile_source (source : string) : string =
+  let lexbuf = Lexing.from_string source in
+  let ast = Parser.programme Lexer.read lexbuf in
+  let ir = Ir_gen.ir_of_ast ast in
+  let asm = Asm_gen.asm_of_ir ir in
+  Emit.string_of_asm asm
+
 let () = Js.export "compile" compile_source
